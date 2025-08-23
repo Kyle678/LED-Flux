868ba959a990b03a9c2c2471ab7d01c387b53851
Kyle Maloney <ktmaloney115@gmail.com>
2025-08-23 06:59:30 -0500

    Started adding controller class and began integrating with database/api

--- backend/LED.py
+++ backend/LED.py
@@ -1,9 +1,12 @@
 from backend.api.app import app
-from backend.database.db_manager import initialize_database
+from backend.database.db_manager import DatabaseManager
+from backend.controller.controller import Controller
 
-class Controller:
-    def __init__ (self, config_file):
-        self.config_file = config_file
-        self.config = self.load_config()
-        initialize_database("ledflux.db")
-        self.app = app
+def main():
+    #initialize_database()
+    controller = Controller('backend/controller/config.ini')
+    controller.fill((255, 0, 0))  # Fill with red color
+    controller.show()
+
+if __name__ == "__main__":
+    main()
--- backend/controller/controller.py
+++ backend/controller/controller.py
@@ -1,14 +1,33 @@
 import json
+import neopixel
+import board
+import configparser
 
 class Controller:
     def __init__ (self, config_file):
         self.config_file = config_file
         self.config = self.load_config()
+        self.pixels = neopixel.NeoPixel(self.pin, self.num_pixels, brightness=self.brightness, auto_write=False)
 
     def load_config(self):
         try:
-            with open(self.config_file, 'r') as file:
-                return json.load(file)
+            config = configparser.ConfigParser()
+            config.read(self.config_file)
+            self.pin_number = config.getint('pixels', 'pin_number', fallback=18)
+            self.pin = board.D18  # Default pin
+            if self.pin_number == 21:
+                self.pin = board.D21
+            if self.pin_number != 18 and self.pin_number != 21:
+                raise ValueError("Invalid pin number. Only 18 and 21 are supported.")
+            self.num_pixels = config.getint('pixels', 'num_pixels', fallback=30)
+            self.brightness = config.getfloat('pixels', 'brightness', fallback=0.5)
+            return config
         except FileNotFoundError:
             return FileNotFoundError("Configuration file not found.")
+        
+    def fill(self, color):
+        self.pixels.fill(color)
+
+    def show(self):
+        self.pixels.show()
         
