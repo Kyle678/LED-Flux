aed9f2247ec7b4552693513bc244354dba5bd247
Kyle Maloney <ktmaloney115@gmail.com>
2025-08-26 22:38:36 -0500

    slight work on animation class. moved color manipulation methods to animations class. started to implement animations.

--- backend/LED.py
+++ backend/LED.py
@@ -8,38 +8,6 @@ import sys
 
 shutdown_event = threading.Event()
 
-def getGradientColors(start_color, end_color, steps):
-    gradient_colors = []
-    for i in range(steps):
-        ratio = i / (steps - 1)
-        r = int(start_color[0] * (1 - ratio) + end_color[0] * ratio)
-        g = int(start_color[1] * (1 - ratio) + end_color[1] * ratio)
-        b = int(start_color[2] * (1 - ratio) + end_color[2] * ratio)
-        gradient_colors.append((r, g, b))
-    return gradient_colors
-
-def getGradient(colorList, steps):
-    gradient_colors = []
-    num_colors = len(colorList)
-    if num_colors < 2:
-        return gradient_colors
-
-    steps_per_segment = steps // (num_colors - 1)
-    for i in range(num_colors - 1):
-        start_color = colorList[i]
-        end_color = colorList[i + 1]
-        segment_colors = getGradientColors(start_color, end_color, steps_per_segment)
-        gradient_colors.extend(segment_colors)
-
-    while len(gradient_colors) < steps:
-        gradient_colors.append(colorList[-1])
-
-    return gradient_colors[:steps]
-
-def rotate_list(lst, n):
-    n = n % len(lst)
-    return lst[-n:] + lst[:-n]
-
 def main():
     print("Starting LED-Flux...")
     controller = Controller('config.ini')
--- backend/animations/animations.py
+++ backend/animations/animations.py
@@ -5,6 +5,34 @@ def rotate(pixels, steps=1):
     steps = steps % length
     return pixels[-steps:] + pixels[:-steps]
 
+def get_gradient(length, colors, loop = False):
+    num_colors = len(colors)
+    if num_colors < 2: ValueError("Invalid number of colors")
+    print(loop)
+    if loop:
+        colors.append(colors[0])
+        num_colors += 1
+    segment = length / (num_colors - 1)
+    pixels = []
+    for i in range(num_colors - 1):
+        c1 = colors[i]
+        c2 = colors[i + 1]
+        seg_length = round(segment * (i + 1), 0) - round(segment * i, 0)
+        for j in range(int(round(seg_length, 0))):
+            pixels.append(mixIndex(c1, c2, seg_length, j))
+    return pixels
+        
+def mixIndex(c1, c2, length, index):
+    weight = index / (length - 1)
+    color = mixColors(c1, c2, weight)
+    return color
+
+def mixColors(c1, c2, weight):
+    mixed_color = tuple([int(round(
+        c1[i]*(1 - weight) + c2[i]*weight
+        , 0)) for i in range(3)])
+    return mixed_color
+
 class Animation:
     def __init__(self, length, type_, params=None):
         self.length = length
@@ -45,3 +73,12 @@ class RotateAnimation(Animation):
 
 # figure out how to standardize regular animations when setting up the list
 # ie. rotate and static use same setup with different params
+
+def generate_pixels(length, colors=None, gradient=False, loop=True, step=0):
+    if gradient:
+        if not colors or len(colors) < 2:
+            raise ValueError("At least two colors are required for gradient")
+        pixels = get_gradient(length, colors, loop=loop)
+        return pixels
+    #fix step and everything yada yada yada...
+
