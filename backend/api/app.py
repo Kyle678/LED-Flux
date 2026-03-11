47afee85690412a8d6623f1617fe0209ea3e4a2b
Kyle Maloney <ktmaloney115@gmail.com>
2026-03-11 02:29:58 -0500

    added cors

--- backend/api/app.py
+++ backend/api/app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import socket
 import json
 import configparser
 
 app = Flask(__name__)
+CORS(app)
 
 config = configparser.ConfigParser()
 config.read('config.ini')
